f1d4013fca02baf50a69fa222b0c872685c76ce4
trishaSolanki <141935315+trishaSolanki@users.noreply.github.com>
2023-11-23 17:28:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
+
 const extendHex = (shortHex) => {
-  // write your code here
-	 const shortHex = shortHex.replace('#', '').toLoweCase();
+  shortHex = shortHex.replace('#', '').toLowerCase();
 
-	const r = shortHex[0];
-	const g = shortHex[1];
-	const b = shortHex[2];
+  const r = shortHex[0];
+  const g = shortHex[1];
+  const b = shortHex[2];
 
-	const fullHex = '#${r.repeat(2)}${g.repeat(2)}${b.repeat(2)}';
-	return fullHex.toUpperCase();
+  const fullHex = `#${r.repeat(2)}${g.repeat(2)}${b.repeat(2)}`;
+  return fullHex.toUpperCase();
 };
 
 // Do not change the code below.
